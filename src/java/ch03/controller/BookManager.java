d8c4a276c265dbe35a4fb9485018c17d39dfa326
nuboat <nuboat@gmail.com>
2013-12-03 19:40:43 +0700

    Done

--- src/java/ch03/controller/BookManager.java
+++ src/java/ch03/controller/BookManager.java
@@ -2,6 +2,7 @@ package ch03.controller;
 
 import ch03.model.Book;
 import java.util.List;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -14,17 +15,18 @@ public class BookManager {
     static {
         try {
             factory = new Configuration().configure().buildSessionFactory();
-        } catch (Exception e) {
+        } catch (HibernateException e) {
             System.err.println(e);
             throw new ExceptionInInitializerError(e);
         }
     }
 
-    public List list() {
-        Session session = factory.openSession();
+    public List<Book> list() {
+        final Session session = factory.openSession();
         session.beginTransaction();
 
-        List books = session.createQuery("FROM Book").list();
+		@SuppressWarnings("unchecked")
+        final List<Book> books = session.createQuery("FROM Book").list();
         session.getTransaction().commit();
         return books;
     }
